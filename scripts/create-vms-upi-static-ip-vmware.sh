58dc3c96e67ab727b663f104e6897c1c620dedc8
Luiz Chiaretto <luizgustavo@chiaretto.com.br>
2021-07-08 13:59:31 -0300

    added variables and power on vms

--- scripts/create-vms-upi-static-ip-vmware.sh
+++ scripts/create-vms-upi-static-ip-vmware.sh
@@ -13,6 +13,8 @@ HTTPUSER=chiaretto1
 HTTPSERVER=chiaret.to
 HTTPDIR=public_html/tam/
 SSHKEY="ssh-rsa ..."
+DATACENTER="CHIARETTO"
+CLUSTER="HOME"
 
 err() {
     echo; echo;
@@ -139,11 +141,11 @@ log "Cluster ID is ${CLUSTER_ID}"
 TEMPLATE=${CLUSTER_ID}-rhcos
 
 log "Creating folder ${VMWARE_FOLDER}"
-govc folder.create "/CHIARETTO/vm/${VMWARE_FOLDER}"
+govc folder.create "/${DATACENTER}/vm/${VMWARE_FOLDER}"
 
 # Import OVA
 log "Importing ova"
-govc import.ova --ds=SSD --pool=/CHIARETTO/host/HOME/Resources --name=${CLUSTER_ID}-rhcos https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/${OPENSHIFT_VERSION}/latest/rhcos-vmware.x86_64.ova || err "Error when importing OVA"
+govc import.ova --ds=SSD --pool=/${DATACENTER}/host/${CLUSTER}/Resources --name=${CLUSTER_ID}-rhcos https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/${OPENSHIFT_VERSION}/latest/rhcos-vmware.x86_64.ova || err "Error when importing OVA"
 #read -p "Download file: https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/${OPENSHIFT_VERSION}/latest/rhcos-vmware.x86_64.ova \n Import on vmware as: ${CLUSTER_ID}-rhcos\n and type y" -n 1 -r
 
 log "Generating base64 from append.ign"
@@ -158,8 +160,7 @@ govc vm.disk.change -vm "$TEMPLATE" -size 120G
 VM_NAME=bootstrap
 log "Creating ${VM_NAME} vm"
 IPCFG="ip=192.168.0.189::192.168.1.1:255.255.255.0:bootstrap:ens192:none nameserver=8.8.8.8"
-
-govc vm.clone -vm=$TEMPLATE -ds=SSD -folder=${VMWARE_FOLDER} -pool=/CHIARETTO/host/HOME/Resources ${VM_NAME}
+govc vm.clone -vm=$TEMPLATE -ds=SSD -folder=${VMWARE_FOLDER} -pool=/${DATACENTER}/host/${CLUSTER}/Resources ${VM_NAME}
 govc vm.power -off -force ${VM_NAME}
 govc vm.change -vm ${VM_NAME} -cpu-hot-add-enabled -memory-hot-add-enabled
 govc vm.change -vm ${VM_NAME} -c=8 -m=8192
@@ -169,12 +170,13 @@ govc vm.change -vm "${VM_NAME}" -e "guestinfo.ignition.config.data.encoding=base
 govc vm.change -vm="${VM_NAME}" -e="disk.enableUUID=1"
 govc vm.change -vm="${VM_NAME}" -e "guestinfo.afterburn.initrd.network-kargs=${IPCFG}"
 govc vm.network.change --vm "${VM_NAME}" -net.address 00:50:56:00:01:89  -net="VM Network" ethernet-0
+govc vm.power -on ${VM_NAME}
 
 VM_NAME=${CLUSTER_ID}-master-0
 log "Creating ${VM_NAME} vm"
 CONFIG_B64=$(cat master.64)
 IPCFG="ip=192.168.0.190::192.168.1.1:255.255.255.0:${CLUSTER_ID}-master-0:ens192:none nameserver=8.8.8.8"
-govc vm.clone -vm=$TEMPLATE -ds=SSD -folder=${VMWARE_FOLDER} -pool=/CHIARETTO/host/HOME/Resources ${VM_NAME}
+govc vm.clone -vm=$TEMPLATE -ds=SSD -folder=${VMWARE_FOLDER} -pool=/${DATACENTER}/host/${CLUSTER}/Resources ${VM_NAME}
 govc vm.power -off -force ${VM_NAME}
 govc vm.change -vm ${VM_NAME} -cpu-hot-add-enabled -memory-hot-add-enabled
 govc vm.change -vm ${VM_NAME} -c=8 -m=16384
@@ -184,12 +186,13 @@ govc vm.change -vm "${VM_NAME}" -e "guestinfo.ignition.config.data.encoding=base
 govc vm.change -vm="${VM_NAME}" -e="disk.enableUUID=1"
 govc vm.change -vm="${VM_NAME}" -e "guestinfo.afterburn.initrd.network-kargs=${IPCFG}"
 govc vm.network.change --vm "${VM_NAME}" -net.address 00:50:56:00:01:90  -net="VM Network" ethernet-0
+govc vm.power -on ${VM_NAME}
 
 VM_NAME=${CLUSTER_ID}-master-1
 log "Creating ${VM_NAME} vm"
 CONFIG_B64=$(cat master.64)
 IPCFG="ip=192.168.0.191::192.168.1.1:255.255.255.0:${CLUSTER_ID}-master-1:ens192:none nameserver=8.8.8.8"
-govc vm.clone -vm=$TEMPLATE -ds=SSD -folder=${VMWARE_FOLDER} -pool=/CHIARETTO/host/HOME/Resources ${VM_NAME}
+govc vm.clone -vm=$TEMPLATE -ds=SSD -folder=${VMWARE_FOLDER} -pool=/${DATACENTER}/host/${CLUSTER}/Resources ${VM_NAME}
 govc vm.power -off -force ${VM_NAME}
 govc vm.change -vm ${VM_NAME} -cpu-hot-add-enabled -memory-hot-add-enabled
 govc vm.change -vm ${VM_NAME} -c=8 -m=16384
@@ -199,12 +202,13 @@ govc vm.change -vm "${VM_NAME}" -e "guestinfo.ignition.config.data.encoding=base
 govc vm.change -vm="${VM_NAME}" -e="disk.enableUUID=1"
 govc vm.change -vm="${VM_NAME}" -e "guestinfo.afterburn.initrd.network-kargs=${IPCFG}"
 govc vm.network.change --vm "${VM_NAME}" -net.address 00:50:56:00:01:91  -net="VM Network" ethernet-0
+govc vm.power -on ${VM_NAME}
 
 VM_NAME=${CLUSTER_ID}-master-2
 log "Creating ${VM_NAME} vm"
 CONFIG_B64=$(cat master.64)
 IPCFG="ip=192.168.0.192::192.168.1.1:255.255.255.0:${CLUSTER_ID}-master-2:ens192:none nameserver=8.8.8.8"
-govc vm.clone -vm=$TEMPLATE -ds=SSD -folder=${VMWARE_FOLDER} -pool=/CHIARETTO/host/HOME/Resources ${VM_NAME}
+govc vm.clone -vm=$TEMPLATE -ds=SSD -folder=${VMWARE_FOLDER} -pool=/${DATACENTER}/host/${CLUSTER}/Resources ${VM_NAME}
 govc vm.power -off -force ${VM_NAME}
 govc vm.change -vm ${VM_NAME} -cpu-hot-add-enabled -memory-hot-add-enabled
 govc vm.change -vm ${VM_NAME} -c=8 -m=16384
@@ -214,3 +218,4 @@ govc vm.change -vm "${VM_NAME}" -e "guestinfo.ignition.config.data.encoding=base
 govc vm.change -vm="${VM_NAME}" -e="disk.enableUUID=1"
 govc vm.change -vm="${VM_NAME}" -e "guestinfo.afterburn.initrd.network-kargs=${IPCFG}"
 govc vm.network.change --vm "${VM_NAME}" -net.address 00:50:56:00:01:92  -net="VM Network" ethernet-0
+govc vm.power -on ${VM_NAME}
